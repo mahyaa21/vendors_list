905ced89f301f5b3cff0951dedafbcd20d317a9a
mahya <m.kabiri@shab.ir>
2020-12-09 15:12:52 +0330

    UPDATE: add style to vendor list

--- vendors_list/src/components/vendorsList/vendorsList.js
+++ vendors_list/src/components/vendorsList/vendorsList.js
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from 'react';
 import {
     useSelector
 } from "react-redux";
+import "./vendorsList.scss"
 const _VendorsList = (props: Props) => {
     const vendors = useSelector(state => state.vendor.vendorList)
     return <>
         {
-           vendors && vendors.data.finalResult.map(vendor => {
+            vendors && vendors.data.finalResult.map(vendor => {
                 if (vendor.type === "VENDOR")
-                    return <div key={vendor.data.id}>{vendor.data.title}</div>
+                    return <div key={vendor.data.id} className="vendor-container">
+                        <div className="text-wrapper">
+                            <div className="vendor-title">{vendor.data.title}</div>
+                            <div className="vendor-description">{vendor.data.description}</div>
+                        </div>
+                        <div className="image-wrapper"><img src={vendor.data.backgroundImage} className="vendor-image" /></div>
+                    </div>
             })
         }</>
 }
